63303515973ea756df2d6edfece031f238a132a4
prince-inument <zahurul.inument@bkash.com>
2024-03-07 14:15:51 +0600

    Add role in JWT

--- spring-authorization-server/src/main/java/com/baeldung/config/DefaultSecurityConfig.java
+++ spring-authorization-server/src/main/java/com/baeldung/config/DefaultSecurityConfig.java
@@ -1,21 +1,31 @@
 package com.baeldung.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +36,7 @@ public class DefaultSecurityConfig {
     SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-            .oidc(withDefaults());    // Enable OpenID Connect 1.0
+                .oidc(withDefaults());    // Enable OpenID Connect 1.0
         return http.formLogin(withDefaults()).build();
     }
 
@@ -34,21 +44,46 @@ public class DefaultSecurityConfig {
     @Order(2)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
-                .authenticated())
-            .formLogin(withDefaults());
+                        .authenticated())
+                .formLogin(withDefaults());
         return http.build();
     }
 
     @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+        return (context) -> {
+            if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+                context.getClaims().claims((claims) -> {
+                    Set<String> roles = AuthorityUtils.authorityListToSet(context.getPrincipal().getAuthorities())
+                            .stream()
+                            .map(c -> c.replaceFirst("^ROLE_", ""))
+                            .collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
+                    claims.put("roles", roles);
+                });
+            }
+        };
+    }
+
+
+    @Bean
     UserDetailsService users() {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails user = User.builder()
-            .username("admin")
-            .password("password")
-            .passwordEncoder(encoder::encode)
-            .roles("USER")
-            .build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails user1 = User.builder()
+                .username("admin")
+                .password("password")
+                .passwordEncoder(encoder::encode)
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.builder()
+                .username("mapp1")
+                .password("pass1")
+                .passwordEncoder(encoder::encode)
+                .roles("MAPP")
+                .build();
+        List<UserDetails> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        return new InMemoryUserDetailsManager(userList);
     }
 
 }
